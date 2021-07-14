a93349f56111f27c6a4499d7265b915d53d33a91
yaowuliu <193792lyw$>
2021-07-14 10:55:40 +0800

    1818. 绝对差值和 2021-07-14

--- arithmetic/1818/main.go
+++ arithmetic/1818/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-
+	arr := sort.IntSlice([]int{2, 4, 6, 8, 10})
+	fmt.Println(arr.Search(1))
+	fmt.Println(arr.Search(5))
+	fmt.Println(arr.Search(8))
+	fmt.Println(arr.Search(12))
 }
 
 func minAbsoluteSumDiff(nums1, nums2 []int) int {
